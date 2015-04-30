35fdae622e9090ed2ab9e24ed0b833332a212ad6
wasteland540 <m_m_elz@web.de>
2015-04-30 09:45:53 +0200

    DateTimeExtensions and BaseViewModel added.

---
+++ ElzUtilLibary/Dates/DateTimeExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ElzUtilLibary.Dates
+{
+    public static class DateTimeExtensions
+    {
+        public static List<DateTime> GetDayRange(this DateTime start, DateTime end)
+        {
+            var dayRange = new List<DateTime>();
+
+            for (int d = 0; d <= (end.DayOfYear - start.DayOfYear); d++)
+            {
+                dayRange.Add(start.AddDays(d));
+            }
+
+            return dayRange;
+        }
+
+        public static bool AreDayRangesConflicted(this IEnumerable<DateTime> dayRange, List<DateTime> dayRange2)
+        {
+            bool isConflicted = false;
+
+            foreach (DateTime date1 in dayRange)
+            {
+                if (dayRange2.Any(date2 => date1.Date == date2.Date))
+                {
+                    isConflicted = true;
+                }
+            }
+
+            return isConflicted;
+        }
+    }
+}
---
+++ ElzUtilLibary/Wpf/ViewModels/BaseViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace ElzUtilLibary.Wpf.ViewModels
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+    }
+}
